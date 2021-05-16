b9c4d44d2490ae1e9bdc1fe8119b2e591b0d0b80
Centrilogic_Chunggil <cghan@centrilogic.com>
2021-05-16 16:34:47 -0400

    Add project files.

---
+++ WeatherData/WeatherData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather
+{
+    public class WeatherData
+    {
+        public static IEnumerable<WeatherObservation> ReadRange(
+            TextReader text,
+            DateTime? start = null,
+            DateTime? end = null,
+            Action<string> errorHandler = null)
+        {
+            return
+                    ReadAll(text, errorHandler)
+                        .SkipWhile((wo) => wo.TimeStamp < (start ?? DateTime.MinValue))
+                        .TakeWhile((wo) => wo.TimeStamp <= (end ?? DateTime.MaxValue));
+        }
+
+        public static IEnumerable<WeatherObservation> ReadAll(TextReader text, Action<string> errorHandler = null)
+        {
+            string line = null;
+
+            while((line = text.ReadLine()) != null)
+            {
+                if(WeatherObservation.TryParse(line, out WeatherObservation wo))
+                {
+                    yield return wo;
+                }
+                else
+                {
+                    try
+                    {
+                        errorHandler?.Invoke(line);
+                    }
+                    catch { }
+                }
+            }
+        }
+
+    }
+}
